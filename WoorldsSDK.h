eb71e87afcf8e7525d4c2e3806770a5b91502899
Yehonatan Levi <leviyehonatan@gmail.com>
2015-04-20 11:40:03 +0300

    update

--- WoorldsSDK.h
+++ WoorldsSDK.h
@@ -9,5 +9,24 @@
 #ifndef Pods_WoorldsSDK_h
 #define Pods_WoorldsSDK_h
 
+#import <CoreLocation/CoreLocation.h>
+
+@protocol WoorldsSDKDelegate
+@required
+-(void)woorldsDataDidUpdate:(NSDictionary *)data;
+@end
+
+
+@interface WoorldsSDK : NSObject <CLLocationManagerDelegate>
+@property (nonatomic, assign) id <WoorldsSDKDelegate> delegate;
+- (id) init;
++ (instancetype)sharedInstance;
+- (NSArray*)getSegmentation:(NSString*)campaignId;
+- (void)trackDownload:(NSString*)campaignId;
+- (void)trackInstall:(NSString*)campaignId;
+- (void)trackClick:(NSString*)campaignId;
+- (void)trackAction:(NSString*)campaignId;
+
+@end
 
 #endif
